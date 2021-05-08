4366783e1e1276bff5efde0151fbb8e1cdc8f40c
unknown <ahadukebede@gmail.com>
2021-05-08 18:32:53 -0400

    beta version 1.0.5

--- bot.py
+++ bot.py
@@ -2,16 +2,15 @@
 import discord #discord library
 from discord.ext import commands #commands
 
-prefix = "!" #command prefix
 
 #Client
 client = commands.Bot(command_prefix="!") #the discord bot, also sets a prefix as !
 
 # EVENTS
 
-# Execute this code block when bot connects to server
+
 @client.event
-async def on_ready(): 
+async def on_ready(): #Excecute when bot joins server 
     print("Bot is running!")
 
 @client.event
@@ -21,24 +20,17 @@ async def on_disconnect():
 
 #aboutme command
 @client.command(name="aboutme") #run run if prefix + "aboutme" is sent
-async def aboutme(context): #the 'context' or arguments after the command is sent. function name MUST be the same as context
+async def aboutme(context):
     print("bot info was requested")
     aboutEmbed = discord.Embed(title="Fuji-San", description="A work in progress discord bot.", color=0x00ff00) #basic embed structure. color is self explanatory. make sure to use hexadecimal code.
-    aboutEmbed.add_field(name="Github: ", value="https://www.github.com/ahoodatheguy", inline=False)
-    aboutEmbed.add_field(name="Version:", value="Beta v1.0.0", inline=False)
+    aboutEmbed.add_field(name="Github: ", value="https://www.github.com/ahoodatheguy/fujiSan", inline=False)
+    aboutEmbed.add_field(name="Version:", value="Beta v1.0.5", inline=False)
     aboutEmbed.set_author(name="ahoodatheguy", url="https://twitter.com/freeahooda", icon_url="https://pbs.twimg.com/profile_images/1369100469075853314/cjg-yHFL_400x400.jpg")
     aboutEmbed.set_footer(text="ありがとう")
     channel = context.message.channel #assigns the channel where the message is sent to variable channel
     await channel.send(embed=aboutEmbed) #send embed
-#credits command
-@client.command(name="credits")
-async def credits(context): #run when command credits is sent
-        print("Credits were requested")
-        channel = context.message.channel # get the channel the command was sent in
-        await channel.send("Created by Ahooda")
-        await channel.send("Check out his github here: https://www.github.com/ahoodatheguy")
-        
-@client.command(name="socials")
+
+@client.command(name="socials") 
 async def credits(context):
     print("Socials were requested")
     channel = context.message.channel
@@ -48,5 +40,44 @@ async def credits(context):
     socialsEmbed.add_field(name="Twitter:", value="twitter.com/freeahooda", inline=False)
     socialsEmbed.add_field(name="Youtube:", value="youtube.com/channel/UCGcwxAVTRuHmrFrI8cwiC3Q")
     await channel.send(embed=socialsEmbed)
-# Connect to server and run bot
+
+@client.command(name="ban")
+@commands.has_permissions(ban_members=True)
+async def ban(context,user : discord.Member,*,reason= "reason not given"): #Ban user and tell the chat
+    channel = context.message.channel
+    await user.ban(reason=reason)
+    if reason == "reason not given":
+        print("user " + str(user) + " was banned")
+        await channel.send(str(user) + " has been banned")
+    else:
+        await channel.send(str(user) + " has been banned for " + str(reason))
+        print("user " + str(user) + " was banned for " + str(reason))
+
+@client.command(name = "vindicate") #unban a user
+@commands.has_permissions(ban_members=True)
+async def unban(context,*, user):
+    channel = context.message.channel
+    bannedUsers = await context.guild.bans()
+    dName, dDiscriminator = user.split('#')
+
+    for banned in bannedUsers:
+        user = banned.user
+        
+        if (user.name, user.discriminator) == (dName, dDiscriminator):
+            await context.guild.unban(user)
+            print(user.name + user.discriminator + " was unbanned")
+            await channel.send("user " + str(user) + " was unbanned")
+@client.command(name="kick")
+@commands.has_permissions(kick_members=True)
+async def kick(context, user : discord.Member,*, reason = "reason not given"):
+    channel = context.message.channel
+    await user.kick(reason=reason)
+    if reason == "reason not given":
+        print("user " + str(user) + " was kicked")
+        await channel.send(str(user) + " was kicked.")
+    else:
+        print("user " + str(user) + " was kicked")
+        await channel.send(str(user) + " was kicked for " + str(reason))
+
+ # Connect to server and run bot
 client.run("YOUR TOKEN HERE")
