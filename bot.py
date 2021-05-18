43f6344f44f8393d6aa738d6494c8111a93fe73a
ahoodatheguy <ahadukebede@gmail.com>
2021-05-18 17:57:24 -0400

    moved every command to their respective cogs.

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ import os
 from discord.ext import commands #commands
 
 
-TOKEN = "YOUR TOKEN HERE"
+TOKEN = "ODMyMzcwODE5OTkyNzgwODEw.YHizvA.dw2f1HL3vnlEj4ojNpJf-0BC82U"
 GITHUB = "https://www.github.com/ahoodatheguy/fujiSan"
-VERSION = "Beta 1.1.2"
+VERSION = "Beta 1.2.0"
 TWITTER = "https://twitter.com/freeahooda"
 YOUTUBE = "youtube.com/channel/UCGcwxAVTRuHmrFrI8cwiC3Q"
 TWITCH = "twitch.tv/ahoodatheguy"
@@ -26,34 +26,6 @@ async def on_ready(): #Excecute when bot joins server
 			except Exception as e:
 				print(f'Failed to load {filename}')
 				print(e)
-
-
-@client.event
-async def on_disconnect():
-	print("bot has disconnected")
-
-#aboutme command
-@client.command(name="aboutme") #run run if prefix + "aboutme" is sent
-async def aboutme(context):
-	print("bot info was requested")
-	aboutEmbed = discord.Embed(title="Fuji-San", description="A work in progress discord bot.", color=0xffb7c5) #basic embed structure. color is self explanatory. make sure to use hexadecimal code.
-	aboutEmbed.add_field(name="Github: ", value=GITHUB, inline=False)
-	aboutEmbed.add_field(name="Version:", value=VERSION, inline=False)
-	aboutEmbed.set_author(name="ahoodatheguy", url=TWITTER, icon_url="https://pbs.twimg.com/profile_images/1369100469075853314/cjg-yHFL_400x400.jpg")
-	aboutEmbed.set_footer(text="ありがとう")
-	channel = context.message.channel #assigns the channel where the message is sent to variable channel
-	await context.reply(embed=aboutEmbed) #send embed
-
-@client.command(name="socials")
-async def credits(context):
-	print("Socials were requested")
-	channel = context.message.channel
-	socialsEmbed = discord.Embed(title="Social Media", description="You can find me here.", color=0xffb7c5)
-	#Replace my socials with yours
-	socialsEmbed.add_field(name="Twitch:", value=TWITCH, inline=False)
-	socialsEmbed.add_field(name="Twitter:", value=TWITTER, inline=False)
-	socialsEmbed.add_field(name="Youtube:", value=YOUTUBE)
-	await context.reply(embed=socialsEmbed)
 
 # Connect to server and run bot
 client.run(TOKEN)
--- cogs/standard_commands.py
+++ cogs/standard_commands.py
@@ -2,16 +2,26 @@ import discord
 from discord import client
 from discord.ext import commands
 
+VERSION = "Beta 1.2.0"
+TWITTER = "https://twitter.com/freeahooda"
+YOUTUBE = "youtube.com/channel/UCGcwxAVTRuHmrFrI8cwiC3Q"
+TWITCH = "twitch.tv/ahoodatheguy"
+
+
+
 class standard(commands.Cog):
 	def __init__(self, client):
 		self.client = client
+
+	# help command
 	@commands.command()
 	async def help(self, context):
 		print('help was requested')
 		helpEmbed = discord.Embed(title='Fuji-San commands', description='Commands that you can use in this server', color=0xffb7c5)
 		helpEmbed.add_field(name='socials', value='prints social media of the moderator', inline=False)
 		helpEmbed.add_field(name = 'aboutme', value="get info about the bot and it's creator", inline=False)
-
+		
+		# Role-specific help entries
 		if commands.has_permissions(manage_messages=True):
 			helpEmbed.add_field(name='clear <amount>', value='delete specified amount of messages (default is 5)', inline=False)
 		if commands.has_permissions(ban_members=True):
@@ -20,5 +30,29 @@ class standard(commands.Cog):
 		if commands.has_permissions(kick_members=True):
 			helpEmbed.add_field(name = 'kick <user>', value='kick specified user', inline=False)
 		await context.reply(embed=helpEmbed)
+
+	# about me command
+	@commands.command()
+	async def aboutme(self, context):
+		print('bot info was requested')
+		aboutEmbed = discord.Embed = discord.Embed(title='Fuji-San', description='A work in progress discord bot', color=0xffb7c5)
+		aboutEmbed.add_field(name='Github: ', value='https://github.com/ahoodatheguy/fujiSan', inline=False)
+		aboutEmbed.add_field(name='Version: ', value=VERSION, inline=False)
+		aboutEmbed.set_author(name='ahoodatheguy', url=TWITTER, icon_url='https://pbs.twimg.com/profile_images/1369100469075853314/cjg-yHFL_400x400.jpg')
+		aboutEmbed.set_footer(text='ありがとう')
+		await context.reply(embed=aboutEmbed)
+
+	# socials command
+	@commands.command()
+	async def socials(self, context):
+		print('socials were requested')
+		socialsEmbed = discord.Embed(title='Social Media: ', description='You can find me here.', color=0xffb7c5)
+		#Replace my socials with yours
+		socialsEmbed.add_field(name='Twitch: ', value=TWITCH, inline=False)
+		socialsEmbed.add_field(name='Twitter: ', value=TWITTER, inline=False)
+		socialsEmbed.add_field(name='Youtube: ', value=YOUTUBE, inline=False)
+		await context.reply(embed=socialsEmbed)
+
+
 def setup(client):
 	client.add_cog(standard(client))
