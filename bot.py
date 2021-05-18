f8dbab13c607e753c29bd94305eef9b8dc71080a
Ahadu Kebede <ahaduk@archyoga.localdomain>
2021-05-18 10:33:23 -0400

    replaced spaces with the superior tabs

--- bot.py
+++ bot.py
@@ -17,43 +17,43 @@ client.remove_command("help") #unbinds the default help command for us to use
 
 @client.event
 async def on_ready(): #Excecute when bot joins server
-    print("Bot is running!")
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            try:
-                client.load_extension(f'cogs.{filename[:-3]}')
-                print(f'cog {filename} loaded')
-            except Exception as e:
-                print(f'Failed to load {filename}')
-                print(e)
+	print("Bot is running!")
+	for filename in os.listdir('./cogs'):
+		if filename.endswith('.py'):
+			try:
+				client.load_extension(f'cogs.{filename[:-3]}')
+				print(f'cog {filename} loaded')
+			except Exception as e:
+				print(f'Failed to load {filename}')
+				print(e)
 
 
 @client.event
 async def on_disconnect():
-    print("bot has disconnected")
+	print("bot has disconnected")
 
 #aboutme command
 @client.command(name="aboutme") #run run if prefix + "aboutme" is sent
 async def aboutme(context):
-    print("bot info was requested")
-    aboutEmbed = discord.Embed(title="Fuji-San", description="A work in progress discord bot.", color=0xffb7c5) #basic embed structure. color is self explanatory. make sure to use hexadecimal code.
-    aboutEmbed.add_field(name="Github: ", value=GITHUB, inline=False)
-    aboutEmbed.add_field(name="Version:", value=VERSION, inline=False)
-    aboutEmbed.set_author(name="ahoodatheguy", url=TWITTER, icon_url="https://pbs.twimg.com/profile_images/1369100469075853314/cjg-yHFL_400x400.jpg")
-    aboutEmbed.set_footer(text="ありがとう")
-    channel = context.message.channel #assigns the channel where the message is sent to variable channel
-    await context.reply(embed=aboutEmbed) #send embed
+	print("bot info was requested")
+	aboutEmbed = discord.Embed(title="Fuji-San", description="A work in progress discord bot.", color=0xffb7c5) #basic embed structure. color is self explanatory. make sure to use hexadecimal code.
+	aboutEmbed.add_field(name="Github: ", value=GITHUB, inline=False)
+	aboutEmbed.add_field(name="Version:", value=VERSION, inline=False)
+	aboutEmbed.set_author(name="ahoodatheguy", url=TWITTER, icon_url="https://pbs.twimg.com/profile_images/1369100469075853314/cjg-yHFL_400x400.jpg")
+	aboutEmbed.set_footer(text="ありがとう")
+	channel = context.message.channel #assigns the channel where the message is sent to variable channel
+	await context.reply(embed=aboutEmbed) #send embed
 
 @client.command(name="socials")
 async def credits(context):
-    print("Socials were requested")
-    channel = context.message.channel
-    socialsEmbed = discord.Embed(title="Social Media", description="You can find me here.", color=0xffb7c5)
-    #Replace my socials with yours
-    socialsEmbed.add_field(name="Twitch:", value=TWITCH, inline=False)
-    socialsEmbed.add_field(name="Twitter:", value=TWITTER, inline=False)
-    socialsEmbed.add_field(name="Youtube:", value=YOUTUBE)
-    await context.reply(embed=socialsEmbed)
+	print("Socials were requested")
+	channel = context.message.channel
+	socialsEmbed = discord.Embed(title="Social Media", description="You can find me here.", color=0xffb7c5)
+	#Replace my socials with yours
+	socialsEmbed.add_field(name="Twitch:", value=TWITCH, inline=False)
+	socialsEmbed.add_field(name="Twitter:", value=TWITTER, inline=False)
+	socialsEmbed.add_field(name="Youtube:", value=YOUTUBE)
+	await context.reply(embed=socialsEmbed)
 
 # Connect to server and run bot
 client.run(TOKEN)
--- cogs/moderation.py
+++ cogs/moderation.py
@@ -2,56 +2,56 @@ import discord
 from discord.ext import commands
 
 class moderation(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+	def __init__(self, client):
+		self.client = client
 
-    #Kicking
-    @commands.command()
-    @commands.has_permissions(kick_members=True)
-    async def kick(self, context, user : discord.Member,*, reason = 'reason not given'):
-        channel = context.message.channel
-        await user.kick(reason=reason)
-        if reason == 'reason not given':
-            print('user ' + str(user) + ' was kicked')
-            await channel.send('**' + str(user) + ' was kicked**')
-        else:
-            print('user ' + str(user) + ' was kicked for ' + str(reason))
-            await channel.send('**' + str(user) + ' was kicked for ' + str(reason) + '**')
+	#Kicking
+	@commands.command()
+	@commands.has_permissions(kick_members=True)
+	async def kick(self, context, user : discord.Member,*, reason = 'reason not given'):
+		channel = context.message.channel
+		await user.kick(reason=reason)
+		if reason == 'reason not given':
+			print('user ' + str(user) + ' was kicked')
+			await channel.send('**' + str(user) + ' was kicked**')
+		else:
+			print('user ' + str(user) + ' was kicked for ' + str(reason))
+			await channel.send('**' + str(user) + ' was kicked for ' + str(reason) + '**')
 
-    #Banning
-    @commands.command()
-    @commands.has_permissions(ban_members=True)
-    async def ban(self, context, user : discord.Member,*, reason = 'reason not given'):
-        channel = context.message.channel
-        await user.ban(reason=reason)
-        if reason == 'reason not given':
-            print('user ' + str(user) + ' was banned')
-            await channel.send('**' + str(user) + ' has been banned**')
-        else:
-            await channel.send('**' + str(user) + ' has been banned for ' + str(reason) + '**')
-            print('user ' + str(user) + ' was banned for ' + str(reason))
+	#Banning
+	@commands.command()
+	@commands.has_permissions(ban_members=True)
+	async def ban(self, context, user : discord.Member,*, reason = 'reason not given'):
+		channel = context.message.channel
+		await user.ban(reason=reason)
+		if reason == 'reason not given':
+			print('user ' + str(user) + ' was banned')
+			await channel.send('**' + str(user) + ' has been banned**')
+		else:
+			await channel.send('**' + str(user) + ' has been banned for ' + str(reason) + '**')
+			print('user ' + str(user) + ' was banned for ' + str(reason))
 
-    #Unbanning
-    @commands.command()
-    @commands.has_permissions(ban_members=True)
-    async def vindicate (self, context,*, user):
-        channel = context.message.channel
-        banned_users = await context.guild.bans()
-        discord_name, discord_discriminator = user.split('#')
+	#Unbanning
+	@commands.command()
+	@commands.has_permissions(ban_members=True)
+	async def vindicate (self, context,*, user):
+		channel = context.message.channel
+		banned_users = await context.guild.bans()
+		discord_name, discord_discriminator = user.split('#')
 
-        for banned in banned_users:
-            user = banned.user
-            if (user.name, user.discriminator) == (discord_name, discord_discriminator):
-                await context.guild.unban(user)
-                print(user.name + user.discriminator + ' was unbanned')
-                await channel.send('**user ' + str(user) + ' was unbanned**')
+		for banned in banned_users:
+			user = banned.user
+			if (user.name, user.discriminator) == (discord_name, discord_discriminator):
+				await context.guild.unban(user)
+				print(user.name + user.discriminator + ' was unbanned')
+				await channel.send('**user ' + str(user) + ' was unbanned**')
 
-    #Clearing Messages
-    @commands.command()
-    @commands.has_permissions(manage_messages=True)
-    async def clear(self, context, amount=5):
-        print(str(context.author) + ' cleared ' +  str(amount) + ' messages')
-        await context.channel.purge(limit = amount + 1)
+	#Clearing Messages
+	@commands.command()
+	@commands.has_permissions(manage_messages=True)
+	async def clear(self, context, amount=5):
+		print(str(context.author) + ' cleared ' + str(amount) + ' messages')
+		await context.channel.purge(limit = amount + 1)
 
 def setup(client):
-    client.add_cog(moderation(client))
+	client.add_cog(moderation(client))
